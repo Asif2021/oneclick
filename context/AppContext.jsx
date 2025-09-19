5164d9674d49d6861666632fa43c76ad7e0016fa
ramzan617 <asiframzan2021@gmail.com>
2025-09-19 11:56:11 +0500

    feat: implement user data retrieval API and update AppContext for seller role management

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -16,10 +16,11 @@ export const AppContextProvider = (props) => {
     const router = useRouter()
 // we will get data in this object whenever user logged in
     const { user } = useUser();
-
+    // by using getToken we can create multiple token for authentication with our next backend
+    const {getToken} = useAuth();
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
@@ -27,7 +28,14 @@ export const AppContextProvider = (props) => {
     }
 
     const fetchUserData = async () => {
+        try {
+            if(user.publicMetadata.role === "seller"){
+            setIsSeller(true)
+        }
         setUserData(userDummyData)
+        } catch (error) {
+            
+        }
     }
 
     const addToCart = async (itemId) => {
@@ -81,11 +89,13 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if(user){
+            fetchUserData()
+        }
+    }, [user])
 
     const value = {
-        user,
+        user, getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
