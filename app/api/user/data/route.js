340ca45f4c76bdb1209fb4714579c35239c4a00c
ramzan617 <asiframzan2021@gmail.com>
2025-09-19 15:09:21 +0500

    feat: add user data retrieval logic and integrate axios for API calls

--- app/api/user/data/route.js
+++ app/api/user/data/route.js
@@ -1,3 +1,4 @@
+import connectDB from "@/config/db";
 import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
@@ -7,7 +8,7 @@ import { NextResponse } from "next/server";
 export async function GET(request){
     try {
         // integrate the api with frontend part
-        
+        // pending code here
         const {userId} = getAuth(request)
         await connectDB()
         const user = await User.findById(userId)
